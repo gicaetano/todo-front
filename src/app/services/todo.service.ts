5b5981130dfc8febed2676fd60f1385289ebcfd6
Gisele <gisele_sig@hotmail.com>
2023-05-06 17:13:27 -0300

    Project Complete

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Todo } from '../models/todo';
+import { environment } from 'environments/environments';
+import { Observable } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: "root",
+})
+export class TodoService {
+
+  baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient, private snack: MatSnackBar) {}
+
+  findById(id: any): Observable<Todo> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Todo>(url);
+  }
+
+  findAll(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.baseUrl);
+  }
+
+  update(todo: Todo): Observable<Todo> {
+    const url = `${this.baseUrl}/${todo.id}`;
+    return this.http.put<Todo>(url, todo);
+  }
+
+  delete(id: any): Observable<void> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<void>(url);
+  }
+
+  create(todo: Todo):Observable<Todo> {
+    return this.http.post<Todo>(this.baseUrl, todo);
+  }
+
+  message(msg: String): void {
+    this.snack.open(`${msg}`, "OK", {
+      horizontalPosition: "end",
+      verticalPosition: "top",
+      duration: 4000
+    });    
+  }
+}
